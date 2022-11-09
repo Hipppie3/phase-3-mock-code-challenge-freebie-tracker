e219883f03b05e7fb10737817cf1b9a5607dc69b
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2022-11-09 09:44:37 -0700

    commit tree 90bcc0ba9bc514d46bce78e1018b1d9d321d40d2

---
+++ db/migrate/20221109061319_create_freebies.rb
@@ -0,0 +1,12 @@
+
+
+class CreateFreebies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :freebies do |t|
+      t.string :item_name
+      t.integer :value
+      t.integer :company_id, foreign_key: true
+      t.integer :dev_id, foreign_key: true
+    end
+  end
+end
